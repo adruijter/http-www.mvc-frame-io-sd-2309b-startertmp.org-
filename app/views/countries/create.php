5c18ce02610a38ee9a5a73f2cca5743bf55d57a4
adruijter <adruijter@gmail.com>
2024-09-10 07:51:02 +0200

    Initial commit

---
+++ app/views/countries/create.php
@@ -0,0 +1,66 @@
+<?php require_once APPROOT . '/views/includes/header.php'; ?>
+
+
+<!-- Maak een formulier om gegevens in de Country tabel te kunnen opslaan -->
+
+<div class="container">
+  
+  <div class="row mt-3">
+    <div class="col-3"></div>
+    <div class="col-6">
+        <h3><?= $data['title']; ?></h3>
+    </div>
+    <div class="col-3"></div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-3">
+    </div>
+    <div class="col-6">
+        <form action="<?= URLROOT; ?>/countries/create" method="post">
+            <div class="mb-3">
+                <label for="inputNameCountry" class="form-label">Land:</label>
+                <input name="country" type="text" class="form-control" id="inputNameCountry" placeholder="Vul hier de naam van het land in" value="<?php if (isset($data['country'])) { echo $data['country']; } ?>">
+                <div class="errorForm"><?php if (isset($data['countryError'])) { echo $data['countryError']; }; ?></div>
+            </div>
+
+            <div class="mb-3">
+                <label for="inputNameCapitalCity" class="form-label">Hoofdstad:</label>
+                <input name="capitalCity" type="text" class="form-control" id="inputNameCapitalCity" placeholder="Vul hier de naam van de hoofdstad in" value="<?php if (isset($data['capitalCity'])) { echo $data['capitalCity']; } ?>">
+            </div>
+
+            <div class="mb-3">
+                <label for="inputNameContinent" class="form-label">Continent:</label>
+                <input name="continent" type="text" class="form-control" id="inputNameContinent" placeholder="Vul hier de naam van het continent in" value="<?php if (isset($data['continent'])) { echo $data['continent']; } ?>">
+            </div>
+
+            <div class="mb-3">
+                <label for="inputPopulation" class="form-label">Aantal inwoners:</label>
+                <input name="population" type="text" class="form-control" id="inputPolulation" placeholder="Vul hier het aantal mensen in woonachtig in het land" value="<?php if (isset($data['population'])) { echo $data['population']; } ?>">
+            </div>
+
+            <div class="d-grid">
+                <button type="submit" class="btn btn-success">Sla op</button>
+            </div>
+        </form>
+    </div>
+    <div class="col-3">
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col-3"></div>
+    <div class="col-6">
+        <a href="<?= URLROOT; ?>/homepages/index">Homepage</a>        
+    </div>
+    <div class="col-3"></div>
+  </div>
+</div>
+
+
+
+
+
+
+
+<?php require_once APPROOT . '/views/includes/footer.php'; ?>
