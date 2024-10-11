e357a6f37ae999f180152aad4cf246c0bbfec9fa
adruijter <adruijter@gmail.com>
2024-10-11 13:26:42 +0200

    Laatste commit

--- app/controllers/Countries.php
+++ app/controllers/Countries.php
@@ -73,20 +73,13 @@ class Countries extends BaseController
             $data['continent'] = trim($_POST['continent']);
             $data['population'] = trim($_POST['population']);
             $data['zipcode'] = trim($_POST['zipcode']);
-         
-            $data = $this->validateCreateForm($data);
 
+            $data = CountryValidator::validateCountryInputFields($data);
            
             /**
              * Wanneer alle Error-keys uit $data leeg zijn kunnen we wegschrijven naar de database
              */
-            if ( 
-                empty($data['countryError']) 
-                && empty($data['capitalCityError'])
-                && empty($data['continentError'])
-                && empty($data['populationError'])
-                && empty($data['zipcodeError'])
-            ) {
+            if ($data['isViewValid']) {
                 /**
                  * Roep de createCountry methode aan van het countryModel object waardoor
                  * de gegevens in de database worden opgeslagen
@@ -118,49 +111,6 @@ class Countries extends BaseController
             }
         }
         $this->view('countries/create', $data);
-    }
-
-    public function validateCreateForm($data)
-    {
-        /**
-         *  Inspecteer of het veld country is ingevuld
-         */
-        if ( empty($data['country'])) {
-            $data['countryError'] = 'Het is verplicht de naam van een land in te vullen!';
-        }
-        if ( empty($data['capitalCity'])) {
-            $data['capitalCityError'] = 'Het is verplicht de naam van de hoofdstad in te vullen!';
-        }
-        if ( empty($data['continent'])) {
-            $data['continentError'] = 'Het is verplicht de naam van het continent in te vullen!';
-        }
-        if ( empty($data['population'])) {
-            $data['populationError'] = 'Het is verplicht het aantal inwoners van het land in te vullen!';
-        }
-        if ( !filter_var($data['population'], FILTER_VALIDATE_INT)) {
-            $data['populationError'] = 'U kunt alleen positieve gehele getallen invoeren';
-        }
-        if ( 
-            $data['population'] < 0
-            || $data['population'] > 4294967295) {
-            $data['populationError'] = 'U kunt alleen positieve getallen invoeren kleiner dan 4294967295';
-        }
-
-        // var_dump(CONTINENTS);
-        // var_dump($data['continent']);
-        if (!in_array($data['continent'], CONTINENTS)) {
-            $data['continentError'] = 'Dit werelddeel bestaat niet, vervang deze door één uit het selectmenu';
-        }
-
-
-        echo preg_match('/^\d{4}[A-Z]{2}$/', $data['zipcode']);
-        if (!preg_match('/^\d{4}[A-Z]{2}$/', $data['zipcode'])) {
-            $data['zipcodeError'] = 'De ingevoerde postcode heeft geen geldig formaat, probeer het opnieuw';
-        }
-
-
-
-        return $data;
     }
 
     public function update($countryId)
@@ -203,18 +153,12 @@ class Countries extends BaseController
              $data['population'] = trim($_POST['population']);
              $data['zipcode'] = trim($_POST['zipcode']);
           
-             $data = $this->validateCreateForm($data);
+             $data = CountryValidator::validateCountryInputFields($data);
 
              /**
              * Wanneer alle Error-keys uit $data leeg zijn kunnen we wegschrijven naar de database
              */
-            if ( 
-                empty($data['countryError']) 
-                && empty($data['capitalCityError'])
-                && empty($data['continentError'])
-                && empty($data['populationError'])
-                && empty($data['zipcodeError'])
-            ) {
+            if ($data['isViewValid']) {
                 /**
                  * Roep de createCountry methode aan van het countryModel object waardoor
                  * de gegevens in de database worden opgeslagen
--- app/require.php
+++ app/require.php
@@ -7,6 +7,7 @@
     require_once 'libraries/BaseController.php';
     require_once 'libraries/Database.php';
     require_once 'helpers/functions.php';
+    require_once 'validators/CountryValidator.php';
     require_once 'config/config.php';
     require_once 'config/constants.php';
     require_once 'helpers/messages.php';
